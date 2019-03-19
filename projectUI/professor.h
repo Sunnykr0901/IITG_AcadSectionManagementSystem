9263e52627518eaef7ce40388bcf04fe4f302fa6
utkarshjain1508 <utkarshjain1508@gmail.com>
2019-03-20 00:40:58 +0530

    Resolved

--- projectUI/professor.h
+++ projectUI/professor.h
@@ -34,6 +34,17 @@ namespace projectUI {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::SplitContainer^  splitContainer1;
+	protected: 
+	private: System::Windows::Forms::Button^  button8;
+	private: System::Windows::Forms::Button^  button7;
+	private: System::Windows::Forms::Button^  button9;
+	private: System::Windows::Forms::Button^  button6;
+	private: System::Windows::Forms::Button^  button5;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button4;
 
 	private:
 		/// <summary>
@@ -48,15 +59,136 @@ namespace projectUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->splitContainer1 = (gcnew System::Windows::Forms::SplitContainer());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->splitContainer1))->BeginInit();
+			this->splitContainer1->Panel1->SuspendLayout();
+			this->splitContainer1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this->splitContainer1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->splitContainer1->Location = System::Drawing::Point(0, 0);
+			this->splitContainer1->Name = L"splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this->splitContainer1->Panel1->AutoScroll = true;
+			this->splitContainer1->Panel1->Controls->Add(this->button8);
+			this->splitContainer1->Panel1->Controls->Add(this->button7);
+			this->splitContainer1->Panel1->Controls->Add(this->button9);
+			this->splitContainer1->Panel1->Controls->Add(this->button6);
+			this->splitContainer1->Panel1->Controls->Add(this->button5);
+			this->splitContainer1->Panel1->Controls->Add(this->button3);
+			this->splitContainer1->Panel1->Controls->Add(this->button2);
+			this->splitContainer1->Panel1->Controls->Add(this->button1);
+			this->splitContainer1->Panel1->Controls->Add(this->button4);
+			this->splitContainer1->Size = System::Drawing::Size(1019, 617);
+			this->splitContainer1->SplitterDistance = 243;
+			this->splitContainer1->TabIndex = 0;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(25, 90);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(187, 65);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(25, 177);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(187, 65);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(25, 270);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(187, 65);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(25, 358);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(187, 65);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"button4";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(25, 447);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(187, 65);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"button5";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(25, 532);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(187, 65);
+			this->button6->TabIndex = 5;
+			this->button6->Text = L"button6";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(25, 621);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(187, 65);
+			this->button7->TabIndex = 6;
+			this->button7->Text = L"button7";
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->Location = System::Drawing::Point(25, 704);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(187, 65);
+			this->button8->TabIndex = 7;
+			this->button8->Text = L"button8";
+			this->button8->UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this->button9->Location = System::Drawing::Point(25, 790);
+			this->button9->Margin = System::Windows::Forms::Padding(3, 3, 3, 30);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(187, 65);
+			this->button9->TabIndex = 8;
+			this->button9->Text = L"button9";
+			this->button9->UseVisualStyleBackColor = true;
 			// 
 			// professor
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1019, 617);
+			this->Controls->Add(this->splitContainer1);
 			this->Name = L"professor";
 			this->Text = L"professor";
+			this->splitContainer1->Panel1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->splitContainer1))->EndInit();
+			this->splitContainer1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
