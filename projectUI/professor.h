f4d1b21db585ac8e8a4dbe0ba0aa7e4527fadb78
utkarshjain1508 <utkarshjain1508@gmail.com>
2019-03-20 00:52:35 +0530

    Form Initialized

---
+++ projectUI/professor.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace projectUI {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for professor
+	/// </summary>
+	public ref class professor : public System::Windows::Forms::Form
+	{
+	public:
+		professor(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~professor()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// professor
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1019, 617);
+			this->Name = L"professor";
+			this->Text = L"professor";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
